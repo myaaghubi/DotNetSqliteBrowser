0dd63880cd12719dbbe189f83b62600c30a457a8
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2014-03-04 12:52:38 +0330

    adding a class to manage sqliteconnection

---
+++ DotNetSqliteBrowser/GetSQLite.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace DotNetSqliteBrowser
+{
+    public class GetSQLite
+    {
+        private SQLiteConnection sqliteConnection;
+
+        public GetSQLite(string _connection)
+        {
+            sqliteConnection = new SQLiteConnection("Data Source=" + _connection);
+        }
+		
+        private void Open()
+        {
+            if (sqliteConnection != null && sqliteConnection.State != System.Data.ConnectionState.Open)
+                sqliteConnection.Open();
+        }
+
+        private void Close()
+        {
+            if (sqliteConnection.State == System.Data.ConnectionState.Open)
+                sqliteConnection.Close();
+        }
+    }
+}

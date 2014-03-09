47f78f5d7e8943cab15ce4996f42e0b8eb046545
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2014-03-09 19:53:19 +0330

    add addColumn.xaml.cs for addColumn.xaml

---
+++ DotNetSqliteBrowser/addColumn.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DotNetSqliteBrowser
+{
+    /// <summary>
+    /// Interaction logic for addColumn.xaml
+    /// </summary>
+    public partial class addColumn : Window
+    {
+        public addColumn()
+        {
+            InitializeComponent();
+        }
+    }
+}

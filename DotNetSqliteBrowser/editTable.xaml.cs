af144a0893fea0982eeea14189e62cf3ce59d046
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2014-07-13 15:17:47 +0430

    update editTable window

--- DotNetSqliteBrowser/editTable.xaml.cs
+++ DotNetSqliteBrowser/editTable.xaml.cs
@@ -64,26 +64,34 @@ namespace DotNetSqliteBrowser
 
         private void applychange_btn_Click(object sender, RoutedEventArgs e)
         {
-            int changes = 0; // 0 for no change, 1 for change column name, 2 for change column type, 3 for change name/type and 4 for add new column
-            DataTable tableOfColumns = (columns_grd.ItemsSource as DataView).Table;
-            if (!tableOfColumns.Columns.Contains("changes"))
-                tableOfColumns.Columns.Add("changes");
-
-            if ((tableOfColumns.Rows[currentIndex])["name"].ToString() != editablecolumnname_txt.Text.Trim())
+            if (columns_grd.SelectedIndex != -1)
             {
-                (tableOfColumns.Rows[currentIndex])["name"] = editablecolumnname_txt.Text.Trim();
-                changes += 1;
-            }
-            if ((tableOfColumns.Rows[currentIndex])["type"].ToString() != editabledatatype_cbx.SelectionBoxItem.ToString())
-            {
-                (tableOfColumns.Rows[currentIndex])["type"] = editabledatatype_cbx.SelectionBoxItem.ToString();
-                changes += 2;
+                currentIndex = columns_grd.SelectedIndex;
 
+                int changes = 0; // 0 for no change, 1 for change name/type and 2 for add new column
+                DataTable tableOfColumns = (columns_grd.ItemsSource as DataView).Table;
+                checkTableColumns(tableOfColumns);
+
+                if ((tableOfColumns.Rows[currentIndex])["name"].ToString() != editablecolumnname_txt.Text.Trim())
+                {
+                    (tableOfColumns.Rows[currentIndex])["name"] = editablecolumnname_txt.Text.Trim();
+                    changes = 1;
+                }
+                if ((tableOfColumns.Rows[currentIndex])["type"].ToString() != editabledatatype_cbx.SelectionBoxItem.ToString())
+                {
+                    (tableOfColumns.Rows[currentIndex])["type"] = editabledatatype_cbx.SelectionBoxItem.ToString();
+                    changes = 1;
+                }
+                if (changes == 1 && (tableOfColumns.Rows[currentIndex])["oldname"].ToString().Length == 0)
+                {
+                    (tableOfColumns.Rows[currentIndex])["oldname"] = (tableOfColumns.Rows[currentIndex])["name"].ToString();
+                    (tableOfColumns.Rows[currentIndex])["oldtype"] = (tableOfColumns.Rows[currentIndex])["type"].ToString();
+                }
+                if ((tableOfColumns.Rows[currentIndex])["changes"].ToString() != "2")
+                    (tableOfColumns.Rows[currentIndex])["changes"] = changes;
+
+                columns_grd.ItemsSource = tableOfColumns.DefaultView;
             }
-            if ((tableOfColumns.Rows[currentIndex])["changes"].ToString() != "4")
-                (tableOfColumns.Rows[currentIndex])["changes"] = changes;
-            
-            columns_grd.ItemsSource = tableOfColumns.DefaultView;
         }
 
         private void removecolumn_btn_Click(object sender, RoutedEventArgs e)
@@ -91,8 +99,6 @@ namespace DotNetSqliteBrowser
             if (columns_grd.SelectedIndex != -1)
             {
                 DataTable tableOfColumns = (columns_grd.ItemsSource as DataView).Table;
-                if (!tableOfColumns.Columns.Contains("changes"))
-                    tableOfColumns.Columns.Add("changes");
 
                 tableOfColumns.Rows[columns_grd.SelectedIndex].Delete();
                 columns_grd.ItemsSource = tableOfColumns.DefaultView;
@@ -102,16 +108,73 @@ namespace DotNetSqliteBrowser
         private void addcolumn_btn_Click(object sender, RoutedEventArgs e)
         {
             DataTable tableOfColumns = (columns_grd.ItemsSource as DataView).Table;
-            if (!tableOfColumns.Columns.Contains("changes"))
-                tableOfColumns.Columns.Add("changes");
+            checkTableColumns(tableOfColumns);
 
             DataRow newColumn = tableOfColumns.NewRow();
             newColumn["name"] = editablecolumnname_txt.Text.Trim();
             newColumn["type"] = editabledatatype_cbx.SelectionBoxItem.ToString();
-            newColumn["changes"] = "4";
+            newColumn["changes"] = "2";
 
             tableOfColumns.Rows.Add(newColumn);
             columns_grd.ItemsSource = tableOfColumns.DefaultView;
+        }
+
+        private void checkTableColumns(DataTable tableOfColumns_)
+        {
+            if (!tableOfColumns_.Columns.Contains("changes"))
+                tableOfColumns_.Columns.Add("changes");
+            if (!tableOfColumns_.Columns.Contains("oldname"))
+                tableOfColumns_.Columns.Add("oldname");
+            if (!tableOfColumns_.Columns.Contains("oldtype"))
+                tableOfColumns_.Columns.Add("oldtype");
+        }
+
+        private void update_btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable tableOfColumns = (columns_grd.ItemsSource as DataView).Table;
+                checkTableColumns(tableOfColumns);
+
+                string query = "ALTER TABLE " + tableName + " RENAME TO temp_" + tableName + ";";
+                getSqlite.ExecuteNonQuery_(query);
+
+                query = "CREATE TABLE " + tablename_txt.Text.Trim() + "(";
+                foreach (DataRow anyColumn in tableOfColumns.Rows)
+                {
+                    query += anyColumn["name"].ToString() + " " + anyColumn["type"].ToString() + ", ";
+                }
+                query += ");";
+                query = query.Remove(query.LastIndexOf(", "), 2);
+
+                getSqlite.ExecuteNonQuery_(query);
+
+                query = "INSERT INTO " + tablename_txt.Text.Trim() + "(";
+                foreach (DataRow anyColumn in tableOfColumns.Rows)
+                {
+                    if (anyColumn["changes"].ToString() == "1")
+                        query += anyColumn["name"].ToString() + ", ";
+                }
+                query = query.Remove(query.LastIndexOf(", "), 2);
+                query += ") SELECT ";
+                foreach (DataRow anyColumn in tableOfColumns.Rows)
+                {
+                    if (anyColumn["changes"].ToString() == "1")
+                        query += anyColumn["oldname"].ToString() + ", ";
+                }
+                query = query.Remove(query.LastIndexOf(", "), 2);
+                query += "FROM temp_" + tableName + " ;";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            this.Close();
+        }
+
+        private void cancel_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }

31d362961c9e72c1e08cd59e3b08d2c6f3ea093b
Mohammad Yaghobi <m.yaghobi.abc@gmail.com>
2014-07-15 03:05:24 +0430

    attach license to source files

--- DotNetSqliteBrowser/Properties/AssemblyInfo.cs
+++ DotNetSqliteBrowser/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿/*
+ * DotNetSqliteBrowser 
+ * Copyright (C) 2014 "Mohammad Yaghobi Beyrami" m.yaghobi.abc@gmail.com
+
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,5 +69,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.56.0.0")]
+[assembly: AssemblyFileVersion("0.56.0.0")]
